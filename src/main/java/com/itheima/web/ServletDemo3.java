83b8eb278ec4e3bf71c2aaf638f276d3707bf363
hyb2419805602 <2419805602@qq.com>
2023-02-04 22:13:16 +0800

    home ubuntu16 idea

--- src/main/java/com/itheima/web/ServletDemo3.java
+++ src/main/java/com/itheima/web/ServletDemo3.java
@@ -1,4 +1,27 @@
 package com.itheima.web;
 
-public class ServletDemo3 {
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class ServletDemo3 extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        // 使用request对象，获取请求数据
+        String name = req.getParameter("name"); // url?name=zhangsan
+
+        // 使用response对象，设置响应数据
+        resp.setHeader("content-type", "text/html; charset=utf-8");
+        resp.getWriter().write("h1" + name + ", 欢迎你！ </h1>");
+
+
+
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        System.out.println("Post...");
+    }
 }
