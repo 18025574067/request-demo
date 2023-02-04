4dd3c0a35d9866e34a69f01ee287484b1680f22b
hyb2419805602 <2419805602@qq.com>
2023-02-04 21:36:31 +0800

    home ubuntu16 idea

---
+++ src/main/java/com/itheima/web/ServletDemo1.java
@@ -0,0 +1,29 @@
+package com.itheima.web;
+
+import javax.jws.WebService;
+import javax.servlet.*;
+import javax.servlet.annotation.WebServlet;
+import java.io.IOException;
+
+@WebServlet("/demo1")
+public class ServletDemo1 implements Servlet {
+    public void init(ServletConfig servletConfig) throws ServletException {
+
+    }
+
+    public ServletConfig getServletConfig() {
+        return null;
+    }
+
+    public void service(ServletRequest servletRequest, ServletResponse servletResponse) throws ServletException, IOException {
+        System.out.println("servlet hello...");
+    }
+
+    public String getServletInfo() {
+        return null;
+    }
+
+    public void destroy() {
+
+    }
+}
