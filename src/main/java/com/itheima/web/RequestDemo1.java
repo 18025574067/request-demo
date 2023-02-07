ed790b32b854ea9dfaec1be2e6217dbfc678f951
hyb <2419805602@qq.com>
2023-02-07 14:59:46 +0800

    office ubuntu16 idea

--- src/main/java/com/itheima/web/RequestDemo1.java
+++ src/main/java/com/itheima/web/RequestDemo1.java
@@ -5,6 +5,7 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.BufferedReader;
 import java.io.IOException;
 
 @WebServlet("/req1")
@@ -31,10 +32,23 @@ public class RequestDemo1 extends HttpServlet {
         String queryString = req.getQueryString();
         System.out.println(queryString);
 
+
+        // ---------------
+        // 获取请求头： user-agent: 浏览的版本信息
+        String agent = req.getHeader("user-agent");
+        System.out.println(agent);
+
     }
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        // 获取post 请求体：请求参数
+
+        // 1. 获取字节输入流
+        BufferedReader br = req.getReader();
+        // 2. 读取数据
+        String line = br.readLine();
+        System.out.println(line);
 
     }
 }
