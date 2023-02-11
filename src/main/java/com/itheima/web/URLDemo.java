f6628a59c9a6249062cdccd1b92a2ff8cad3f473
hyb <2419805602@qq.com>
2023-02-11 17:49:59 +0800

    office ubuntu16 idea

--- src/main/java/com/itheima/web/URLDemo.java
+++ src/main/java/com/itheima/web/URLDemo.java
@@ -1,13 +1,33 @@
 package com.itheima.web;
 
 import java.io.UnsupportedEncodingException;
+import java.net.URLDecoder;
 import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
 
 public class URLDemo {
     public static void main(String[] args) throws UnsupportedEncodingException {
         String username = "张三";
 
+        // 1. URL编码
         String encode = URLEncoder.encode(username, "UTF-8");
         System.out.println(encode);
+
+        System.out.println("================");
+        // 2. URL解码
+//        String decode = URLDecoder.decode(encode, "UTF-8"); // 张三
+        String decode = URLDecoder.decode(encode, "ISO-8859-1"); // å¼ ä¸
+        System.out.println(decode);
+
+        // 3. 转换为字节数组
+        byte[] bytes = decode.getBytes("ISO-8859-1");
+//        for (byte b : bytes) {
+//            System.out.print(b + " ");
+//        }
+
+        // 4. 将字节数组转换为字符串
+        String s = new String(bytes, "UTF-8");
+        System.out.println(s);
+
     }
 }
