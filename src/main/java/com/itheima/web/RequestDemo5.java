e781c9a71ab0bf423321fe790456920439be5a0e
hyb2419805602 <2419805602@qq.com>
2023-02-15 23:22:41 +0800

    home ubuntu16 idea

---
+++ src/main/java/com/itheima/web/RequestDemo5.java
@@ -0,0 +1,29 @@
+package com.itheima.web;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+
+/**
+ * 请求转发
+ */
+@WebServlet("/req5")
+public class RequestDemo5 extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        System.out.println("demo5...");
+
+        // 请求转发
+        req.getRequestDispatcher("/req6").forward(req, resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        this.doGet(req, resp);
+    }
+}
