84bbf772be23350c29307c483a0b17fd8a2226d6
hyb2419805602 <2419805602@qq.com>
2023-02-16 21:54:40 +0800

    home ubuntu16 idea

---
+++ src/main/java/com/itheima/web/response/ResponseDemo1.java
@@ -0,0 +1,33 @@
+package com.itheima.web.response;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * 重定向
+ */
+@WebServlet("/resp1")
+public class ResponseDemo1 extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        System.out.println("resp1...");
+
+        // 重定向
+        // 1. 设置响应状态码：302
+        resp.setStatus(302);
+
+        // 2. 设置响应头：location
+        resp.setHeader("location", "/request-demo/resp2");
+
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        this.doGet(req, resp);
+    }
+}
